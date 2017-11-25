320d108a43d5baae87afcad46724cf7726c5e222
Pekka Lammi <pekka.lammi@aalto.fi>
2017-11-25 20:38:42 +0200

    create mockup api point for example data and populate expanded view

--- front/src/ExpandedFlightRow.js
+++ front/src/ExpandedFlightRow.js
@@ -1,58 +1,45 @@
 import React, { Component } from 'react';
+import mockupExpandedData from './mockupexpandeddata.js'
 
 class ExpandedFlightRow extends Component {
   render() {
+
     return (
-      <div className={this.props.status} style={{'display': 'flex', 'flexDirection': 'row'}}
-        onClick={() => {
-          console.log('yes')
-        }}
-      >
-        <div className='column'>
-          <div className="box">
-            <p className='subtitle'>Flight <b>AYY135</b></p>
-            <ul>
-              <li>PLAN_CARRIER_CODE</li>
-              <li>PLAN_FLIGHT_NUMBER</li>
-              <li>PLAN_FLIGHT_NUMBER_CHAR4</li>
-              <li>PLAN_FLIGHT_SEQ_NUMBER</li>
-              <li>PLAN_DEPARTURE_DATETIME_LOCAL</li>
-              <li>PLAN_DEPARTURE_DATE_LOCAL</li>
-              <li>PLAN_DEPARTURE_TIME_LOCAL</li>
-              <li>PLAN_DEPARTURE_DATETIME_UTC</li>
-              <li>PLAN_DEPARTURE_DATE_UTC</li>
-              <li>PLAN_DEPARTURE_TIME_UTC</li>
-              <li>PLAN_ARRIVAL_DATETIME_LOCAL</li>
-              <li>PLAN_ARRIVAL_DATE_LOCAL</li>
-              <li>PLAN_ARRIVAL_TIME_LOCAL</li>
-              <li>PLAN_ARRIVAL_DATETIME_UTC</li>
-              <li>PLAN_ARRIVAL_DATE_UTC</li>
-              <li>PLAN_ARRIVAL_TIME_UTC</li>
-              <li>PLAN_DEPARTURE_STATION</li>
-              <li>PLAN_ARRIVAL_STATION</li>
-              <li>PLAN_START_OF_OPERATION</li>
-              <li>PLAN_END_OF_OPERATION</li>
-              <li>PLAN_DAYS_OF_OPERATION</li>
-              <li>PLAN_AC_OWNER</li>
-              <li>PLAN_SERVICE_TYPE</li>
-              <li>PLAN_AC_TYPE</li>
-            </ul>
+      <span>
+        <div className={this.props.status} style={{'display': 'flex', 'flexDirection': 'row'}}
+          onClick={() => {
+          }}
+        >
+          <div className='column'>
+            <div className="box">
+              <p className='subtitle'>Flight <b>{ mockupExpandedData[0].PLAN_CARRIER_CODE + mockupExpandedData[0].PLAN_FLIGHT_NUMBER }</b></p>
+              <ul>
+                <li>Seq number: {mockupExpandedData[0].PLAN_FLIGHT_SEQ_NUMBER}</li>
+                <li>Local departure date: {mockupExpandedData[0].PLAN_DEPARTURE_DATETIME_LOCAL}</li>
+                <li>Local arrival date: {mockupExpandedData[0].PLAN_ARRIVAL_DATETIME_LOCAL}</li>
+                <li>Departure airport: {mockupExpandedData[0].PLAN_DEPARTURE_STATION}</li>
+                <li>Arrival airport: {mockupExpandedData[0].PLAN_ARRIVAL_STATION}</li>
+                <li>Aircraft owner: {mockupExpandedData[0].PLAN_AC_OWNER}</li>
+                <li>Aircraft service type: {mockupExpandedData[0].PLAN_SERVICE_TYPE}</li>
+                <li>Aircraft type: {mockupExpandedData[0].PLAN_AC_TYPE}</li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div className='column'>
-          <div className='box'>
-            <p className='subtitle'>Risks</p>
-            <div className="media-content">
-              Some risk information about relevant cause. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <div className='column'>
+            <div className='box'>
+              <p className='subtitle'>Risks</p>
+              <div className="media-content">
+                Some risk information about relevant cause. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </div>
+            </div>
+          </div>
+          <div className='column'>
+            <div className='box'>
+              <p className='subtitle'>Stats</p>
             </div>
           </div>
         </div>
-        <div className='column'>
-          <div className='box'>
-            <p className='subtitle'>Stats</p>
-          </div>
-        </div>
-      </div>
+      </span>
     )
   }
 }
